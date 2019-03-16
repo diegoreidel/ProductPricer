e79aab44030e1efee4a37726aac1de5f3fd301b6
Diego Reidel <diego.reidel@sap.com>
2019-03-16 19:18:07 -0300

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProductPricer'`, () => {
+  it(`should have as title 'Product Pricer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ProductPricer');
+    expect(app.title).toEqual('Product Pricer');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ProductPricer!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Product Pricer!');
   });
 });
